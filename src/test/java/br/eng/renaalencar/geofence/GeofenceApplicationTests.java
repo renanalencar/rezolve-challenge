6bdbb0151f6840008399c956c1142d1e998efa21
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-19 11:08:29 -0300

    first commit

---
+++ src/test/java/br/eng/renaalencar/geofence/GeofenceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eng.renaalencar.geofence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
